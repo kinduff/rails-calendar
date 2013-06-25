d6e8d3a1ef042d4e3011d056832d30d5312191ec
Kinduff <abarcadabra@gmail.com>
2013-06-25 16:43:27 -0500

    Event: Edit, update & destroy. Formato y orden de HTML

--- README.md
+++ README.md
@@ -7,7 +7,6 @@ Proyecto de research con gema *event_calendar* para desarrollo de micrositio de 
 + Administrador de eventos
 + Eventos de un sólo día o de varios días
 + Información de cada evento al hacer clic
-+ Suscripción de evento
 + Vista total del calendario
 
 ## Base de datos
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,4 +13,23 @@ class EventsController < ApplicationController
   def show
     @event = Event.find params[:id]
   end
+  def edit
+    @event = Event.find params[:id]
+  end
+  def update
+    @event = Event.find params[:id]
+    if @event.update_attributes params[:event]
+      redirect_to calendar_path
+    else
+      render :edit
+    end
+  end
+  def destroy
+    event = Event.find params[:id]
+    if event.destroy
+      redirect_to calendar_path
+    else
+      redirect_to calendar_path
+    end
+  end
 end
