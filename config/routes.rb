5a2983d9106c5f873926707b1de547143407fb0b
Kinduff <abarcadabra@gmail.com>
2013-06-25 16:53:01 -0500

    Routes: Root set

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Calendario::Application.routes.draw do
+  root :to => 'calendar#index'
   match '/calendar(/:year(/:month))' => 'calendar#index', :as => :calendar, :constraints => {:year => /\d{4}/, :month => /\d{1,2}/}
   resources :events
 end
